f51257630d145363dfc3a8e110ffb824cc3f7b9b
Temct <150273475+Defferentior@users.noreply.github.com>
2024-01-02 14:44:54 -0800

    chapter 4.7

--- src/lib/components/items/children/ItemList.component.svelte
+++ src/lib/components/items/children/ItemList.component.svelte
@@ -26,10 +26,49 @@
         return css.trim()
     }
 
-    function handleClick() {
+    function handleClick(item: ItemInterface) {
         // send an event to the parent component
         dispatch('selectItem', {
             item
         })
     }
+
+    function handleKeyDown(event: KeyboardEvent, item: ItemInterface) {
+        if (event.key === 'Enter') {
+            handleClick(item);
+        }
+    }
 </script>
+
+<button data-testid={testid} class={cssClass()} on:click={() => handleClick(item)} on:keydown={(event) => handleKeyDown(event, item)}>
+    <div class="selected-indicator">*</div>
+    <div class="name"> {item.name} [{item.selected}]</div>
+</button>
+
+<style>
+    button.item {
+        padding: 5px;
+        outline: solid 1px #eee;
+        display: flex;
+        align-items: center;
+        height: 30px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+
+    }
+    button.item.name{
+        margin-left: 6px;
+    }
+    button.item.selected-indicator {
+        font-size: 2em;
+        line-height: 0.5em;
+        margin: 10px 8px 0 8px;
+        color: lightgray;
+    }
+    button.item.selected.selected-indicator {
+        color: skyblue;
+    }
+    button.item:hover {
+        background-color: #eee;
+    }
+</style>
